f5eb94b5d011d4e7881850fb77f1b1b809921ca0
Vishakhasenthilnathan <vishakhasenthilnathan@gmail.com>
2024-11-01 18:22:50 +0530

    Create new project

---
+++ src/main/java/org/hexad/librarymanagementservice/LibraryManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package org.hexad.librarymanagementservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LibraryManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LibraryManagementServiceApplication.class, args);
+    }
+
+}
